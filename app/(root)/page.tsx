318011b062c1af1710ed027cfc52c9820b71ccea
Dimas Septyanto <gn.dimas@gmail.com>
2023-08-19 14:20:06 +0700

    Integrate authentication using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-    return (
-      <div></div>
-    )
-  }
+const SetupPage = () => {
+  return (
+    <div className="flex flex-col gap-3 items-center justify-center p-10 text-lg font-semibold">
+      <div className="p-3 bg-gray-200 rounded-lg">
+        Root page, protected route
+      </div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default SetupPage
   
