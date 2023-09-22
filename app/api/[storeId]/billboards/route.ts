e80539c394828dbd46dec7dc7659d89e470e99e7
System Administrator <gn.dimas@gmail.com>
2023-09-22 16:20:20 +0700

    Create API for get single billboard, delete billboard and get billboard lists

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server"
 export async function POST(req:Request, {params}: {params: {storeId: string}}) {
     try {
         const {userId} = auth();
-        if(!userId) return new NextResponse('Unauthorized', {status: 401})
+        if(!userId) return new NextResponse('Unauthenticated', {status: 401})
 
         const body = await req.json();
         const {label, imageUrl} = body;
@@ -14,7 +14,15 @@ export async function POST(req:Request, {params}: {params: {storeId: string}}) {
         if(!imageUrl) return new NextResponse('Image URL is required', {status: 400});
         if(!params.storeId) return new NextResponse('Store Id is required', {status: 400})
 
-        const store = await prismadb.billboard.create({
+        const storeByUserId = await prismadb.store.findFirst({
+            where: {
+                id: params.storeId,
+                userId: userId
+            }
+        })
+        if(!storeByUserId) return new NextResponse('Unauthorized', {status: 403})
+
+        const billboard = await prismadb.billboard.create({
             data: {
                 label,
                 imageUrl,
@@ -22,10 +30,28 @@ export async function POST(req:Request, {params}: {params: {storeId: string}}) {
             }
         })
 
-        return NextResponse.json(store);
+        return NextResponse.json(billboard);
 
     } catch (error) {
         console.error(`[BILLBOARD_POST_ERROR]: ${error}`)
+        return new NextResponse('Internal error', {status: 500})
+    }
+}
+
+export async function GET(req:Request, {params}: {params: {storeId: string}}) {
+    try {
+        if(!params.storeId) return new NextResponse('Store Id is required', {status: 400})
+
+        const billboards = await prismadb.billboard.findMany({
+            where: {
+                storeId: params.storeId
+            }
+        })
+
+        return NextResponse.json(billboards);
+
+    } catch (error) {
+        console.error(`[BILLBOARD_GET_ERROR]: ${error}`)
         return new NextResponse('Internal error', {status: 500})
     }
 }
