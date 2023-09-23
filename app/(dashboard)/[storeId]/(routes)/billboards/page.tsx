944eae32f1a0d47230c008cd6815574c34226fd7
System Administrator <gn.dimas@gmail.com>
2023-09-23 07:24:54 +0700

    Initiate data table for billboard list using shadcn-ui

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,11 +1,32 @@
-import BillboardClient from '@/components/BillboardClient'
 import React from 'react'
+import prismadb from '@/lib/prismadb'
+import {format} from 'date-fns'
+import BillboardClient from '@/components/BillboardClient'
+import { BillboardColumn } from '@/components/BillboardColumn'
 
-const Billboards = () => {
+
+
+const Billboards = async ({params}: {params: {storeId: string}}) => {
+  
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((billboard) => ({
+    id: billboard.id,
+    label: billboard.label,
+    createdAt: format(billboard.createdAt, 'do MMMM yyyy')
+  }))
+  
   return (
     <div className='flex-col'>
         <div className='flex-1 space-y-4 p-8 pt-6'>
-            <BillboardClient />
+            <BillboardClient data={formattedBillboards}/>
         </div>
     </div>
   )
